a12cde4c6d0f92114461ef5edf8c11922f8bcd53
2019211001001028ShuYanZhe <367037607@qq.com>
2021-06-02 09:46:32 +0800

    week 14 lab1 and lab2

--- src/main/java/com/ShuYanzhe/controller/UpdateUserServlet.java
+++ src/main/java/com/ShuYanzhe/controller/UpdateUserServlet.java
@@ -3,47 +3,56 @@ package com.ShuYanzhe.controller;
 import com.ShuYanzhe.dao.UserDao;
 import com.ShuYanzhe.model.User;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-@WebServlet(name = "UpdateUserServlet",value = "/updateUser")
+@WebServlet(name = "UpdateUserServlet", value = "/updateUser")
 public class UpdateUserServlet extends HttpServlet {
-    Connection con =null;
     @Override
-    public void init() throws ServletException {
-        con = (Connection) getServletContext().getAttribute("con");
-    }
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
-        String username=request.getParameter("username");
-        String password=request.getParameter("password");
-        String email=request.getParameter("email");
-        Date birthDate= Date.valueOf(request.getParameter("birthDate"));
-        String gender=request.getParameter("gender");
-
-        User user=new User(id,username,password,email,gender,birthDate);
-        UserDao userDao=new UserDao();
-
-        try {
-            userDao.updateUser(con,user);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        HttpSession session=request.getSession();
-        session.setAttribute("user",user);
-        session.setMaxInactiveInterval(30);
-        request.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(request,response);
-    }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("WEB-INF/views/updateUser.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int Id= Integer.parseInt(request.getParameter("Id"));
+        String Username=request.getParameter("Username");
+        String password=request.getParameter("password");
+        String Email=request.getParameter("Email");
+        String Gender=request.getParameter("Gender");
+        try {
+            SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+            Date Date=sdf.parse(request.getParameter("Date"));
+            User user=new User();
+            user.setId(Id);
+            user.setUsername(Username);
+            user.setPassword(password);
+            user.setEmail(Email);
+            user.setGender(Gender);
+            user.setBirthDate(Date);
+            UserDao userDao=new UserDao();
+            Connection con = (Connection) getServletContext().getAttribute("con");
+            try {
+                if(userDao.updateUser(con,user)!=0) {
+                    User user1=userDao.findByUsernamePassword(con,Username,password);
+                    HttpSession session=request.getSession();
+                    session.setMaxInactiveInterval(10);
+                    session.setAttribute("user",user1);
+                    request.getRequestDispatcher("accountDetails").forward(request, response);
+                }
+            } catch (SQLException throwable) {
+                throwable.printStackTrace();
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
